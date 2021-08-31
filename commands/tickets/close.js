cbb4ff9a362bbc541c813c5158908bc00d3a9a82
Foksha <55259471+FokshaWasTaken@users.noreply.github.com>
2021-08-31 15:18:53 +0100

    t!close deletes the channel, but cant t!new anymore
    E

--- commands/tickets/close.js
+++ commands/tickets/close.js
@@ -10,8 +10,8 @@ module.exports = {
 	permissions: 'SEND_MESSAGES',
 	exec: async (client, message, args) => {
 		if(message.channel.name.includes('ticket-')) {
-			const ticket_member = message.guild.members.cache.get(message.channel.name.split('ticket-').join(''));
-			if(message.member.permissions.has('ADMINISTRATOR') || message.channel.name === `ticket-${message.author.id}`) {
+			const ticket_member = (await message.guild.members.fetch()).find(m => m.user.username.toLowerCase() === message.channel.name.slice(7));
+            if(message.member.permissions.has('ADMINISTRATOR') || message.channel.name === `ticket-${message.author.username.toLowerCase()}`) {
 				message.channel.messages.fetch().then(async (messages) => {
 					const output = messages.map(m => `${new Date(m.createdAt).toLocaleString('en-US')} - ${m.author.discriminator}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`).reverse().join('\n');
 
@@ -33,7 +33,7 @@ module.exports = {
 					
 
                     const embed = new MessageEmbed()
-                        .setDescription(`**Here's your freshly baked transcript for ${channel.name}!\n[\📄 View\](${sbin.url})**`)
+                        .setDescription(`**Here's your freshly baked transcript for ${message.channel.name}!\n[\📄 View\](${sbin.url})**`)
                         .setColor('GREEN');
                     ticket_member.user.send({ content: `Here is a transcript of your ticket, please click the link below to vew the transcript`, embeds: [embed] }); 
 				}).then(() => {
