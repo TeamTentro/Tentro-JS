fc6fdf826cd87c462b21eb3adf550bc442454b75
Codeize <mrcodeize@gmail.com>
2021-08-31 23:52:01 +0100

    Added MongoDB!
    Added some baseplate schemas too!

---
+++ database/Schema/Guild.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model("Guild", new mongoose.Schema({
+
+    id: { type: String }, // ID of the guild
+    registeredAt: { type: Number, default: Date.now() },
+    prefix: { type: String, default: "t!" },
+
+    addons: { type: Object, default: { // Extra features data
+        welcome: {
+            enabled: false, // Welcome features are enabled
+            channel:  null, // ID for the channel to send messages to
+            message: null, // Custom message
+            image: false, // Check if image is enabled
+            embed: false // Check if embed is enabled
+        },
+        goodbye: {
+            enabled: false, // Goodbye features are enabled
+            channel:  null, // ID for channel to send messages to
+            message: null, // Custom message
+            image: false, // Check if image is enabled
+            embed: false // Check if embed is enabled
+        }
+    }}
+
+}));
