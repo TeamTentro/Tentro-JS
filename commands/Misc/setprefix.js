b46f09f6c7db9f2553ed161c02caa004562950f8
Tony <ag04498118@edu.mon.bg>
2021-10-28 19:19:27 +0300

    even more error handling

--- commands/Misc/setprefix.js
+++ commands/Misc/setprefix.js
@@ -1,5 +1,6 @@
 const {MessageEmbed} = require('discord.js')
 const { setPrefix } = require("../../utils/prefix-utils.js")
+const {getPrefix} = require("../../utils/prefix-utils.js");
 
 module.exports = {
     name: "setprefix",
@@ -10,16 +11,20 @@ module.exports = {
     permissions: "ADMINISTRATOR",
     exec: async (client, message, args) => {
         let newPrefix = args[0]
+        const currentPrefix = await getPrefix(message.guild?.id) || GuildPrefix
+
+        if (!newPrefix) {
+            return message.channel.send('Couldn\'t detect a valid prefix!')
+        }
 
         if (newPrefix.toLowerCase() === '-default') newPrefix = 't!'
 
-        if (!newPrefix || newPrefix.length > 5) {
-            return message.channel.send('Prefix must be under 5 characters!')
+        if (newPrefix.length > 5) {
+            return message.channel.send('Prefix must be 5 or less characters!')
         }
+
         if (!message.member.permissions.has("ADMINISTRATOR")) return message.reply("You dont have the required permissions to make this change!").then(msg => {
             setTimeout(() => msg.delete(), 5000)})
-
-        if (!newPrefix) return message.reply("Couldn't register valid characters.")
 
         if (newPrefix === 't!') {
             await message.guild.me.setNickname(``)
@@ -27,6 +32,9 @@ module.exports = {
             await message.guild.me.setNickname(`[${newPrefix}] Tentro`)
         }
 
+        if (currentPrefix === newPrefix) {
+            return message.channel.send('That\'s already the bot\'s prefix!')
+        }
 
 
         setPrefix(message.guild.id, newPrefix)
